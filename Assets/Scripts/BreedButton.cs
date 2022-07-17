6163c63213dfb43c75025f00d2b4a12b7cc65d2c
Epord <pedro.balaguer@hotmail.com>
2022-07-16 18:56:36 -0700

    Breedinggit add .

--- Assets/Scripts/BreedButton.cs
+++ Assets/Scripts/BreedButton.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BreedButton : MonoBehaviour
 {
@@ -35,8 +36,24 @@ public class BreedButton : MonoBehaviour
     {
         if (this.input1.IsHoldingItem() && this.input2.IsHoldingItem() && !this.output.IsHoldingItem() && Input.GetMouseButtonDown(0))
         {
+            Dice dice1 = this.input1.holdedItem.GetComponent<Dice>();
+            Dice dice2 = this.input2.holdedItem.GetComponent<Dice>();
+
             GameObject newDice = Instantiate(dicePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            this.BreedDice(dice1.dicefaces, dice2.dicefaces, newDice.GetComponent<Dice>());
+
             this.output.AssignItem(newDice.GetComponent<Grabbable>());
         }
+    }
+
+    private void BreedDice(DiceFace[] daddy, DiceFace[] mommy, Dice result)
+    {
+        DiceFace[] daddyClone = (DiceFace[])daddy.Clone();
+        DiceFace[] mommyClone = (DiceFace[])mommy.Clone();
+
+        Array.Sort(daddyClone, (x, y) => UnityEngine.Random.RandomRange(-10, 10));
+        Array.Sort(mommyClone, (x, y) => UnityEngine.Random.RandomRange(-10, 10));
+
+        result.dicefaces = new DiceFace[] { daddyClone[0], daddyClone[1], daddyClone[2], mommyClone[0], mommyClone[1], mommyClone[2]};
     }
 }
--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -5,14 +5,18 @@ using UnityEngine.UIElements;
 
 public class Dice : MonoBehaviour
 {
+    [SerializeField]
     public DiceFace[] dicefaces;
 
     public DiceFace selectedFace;
 
-// Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        this.dicefaces = new DiceFace[6];
+        for (int i = 0; i < 6; i++)
+        {
+            this.dicefaces[i] = new DiceFace(Random.RandomRange(0, 10), Random.RandomRange(0, 10));
+        }
     }
 
     // Update is called once per frame
@@ -20,4 +24,5 @@ public class Dice : MonoBehaviour
     {
         
     }
+
 }
